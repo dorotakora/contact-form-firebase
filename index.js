db4465f52838ae25b8d5a30f31c05ed9f41f09b3
dorotakora <dorota.kora@o2.pl>
2019-06-20 16:33:22 +0200

    firstcommit

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const config = {
-    apiKey: "AIzaSyD2Ay1XOIDUQGQWolVVzTo1zEF0ukBU0SI",
-    authDomain: "contact-form-8b683.firebaseapp.com",
-    databaseURL: "https://contact-form-8b683.firebaseio.com",
-    projectId: "contact-form-8b683",
-    storageBucket: "contact-form-8b683.appspot.com",
-    messagingSenderId: "993390721195",
-    appId: "1:993390721195:web:93eabd0f689c266d"
+var config = {
+    apiKey: "AIzaSyA0225-Fr1pp1xMUM_LzVhDT5rNvcX3x34",
+    authDomain: "contact-form-firebase-9c271.firebaseapp.com",
+    databaseURL: "https://contact-form-firebase-9c271.firebaseio.com",
+    projectId: "contact-form-firebase-9c271",
+    storageBucket: "contact-form-firebase-9c271.appspot.com",
+    messagingSenderId: "538006884949",
+    appId: "1:538006884949:web:2864715cb95f9557"
 };
 firebase.initializeApp(config);
 
-//Reference messages collection
+// Reference messages collection
 let messagesRef = firebase.database().ref('messages');
 
-
 // Listen for form submit
-document.getElementById('contact-form').addEventListener('submit', submitForm);
+document.getElementById('contactForm').addEventListener('submit', submitForm);
 
-//Submit form
-function submitForm(e) {
+// Submit form
+function submitForm(e){
     e.preventDefault();
-    // Get value
+
+    // Get values
     let name = getInputVal('name');
     let company = getInputVal('company');
     let email = getInputVal('email');
@@ -27,36 +27,35 @@ function submitForm(e) {
     let message = getInputVal('message');
 
     // Save message
-    saveMessage(name,company,email,phone,message);
+    saveMessage(name, company, email, phone, message);
 
     // Show alert
     document.querySelector('.alert').style.display = 'block';
 
-    //Hide after 3 second
+    // Hide alert after 3 seconds
     setTimeout(function(){
         document.querySelector('.alert').style.display = 'none';
     },3000);
 
-    // Clear input fields
-    document.getElementById('contact-form').reset();
+    // Clear form
+    document.getElementById('contactForm').reset();
 }
 
-// function to get form values
-
-function getInputVal(id) {
+// Function to get get form values
+function getInputVal(id){
     return document.getElementById(id).value;
 }
 
 // Save message to firebase
-function saveMessage(name, company,email,phone,message) {
+function saveMessage(name, company, email, phone, message){
     let newMessageRef = messagesRef.push();
     newMessageRef.set({
         name: name,
-        company: company,
-        email: email,
-        phone: phone,
-        message: message
-    })
+        company:company,
+        email:email,
+        phone:phone,
+        message:message
+    });
 }
 
 
